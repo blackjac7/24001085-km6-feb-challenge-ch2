64ef9383ea3a8e03b9e7fc04d246c20e1a7af121
blackjac7 <febriansyah705@gmail.com>
2024-03-08 23:53:40 +0700

    add cars page and filter implementation

--- public/scripts/app.example.js
+++ public/scripts/app.example.js
@@ -1,37 +1,152 @@
 class App {
-  constructor() {
-    this.clearButton = document.getElementById("clear-btn");
-    this.loadButton = document.getElementById("load-btn");
-    this.carContainerElement = document.getElementById("cars-container");
-  }
-
-  async init() {
-    await this.load();
-
-    // Register click listener
-    this.clearButton.onclick = this.clear;
-    this.loadButton.onclick = this.run;
-  }
-
-  run = () => {
-    Car.list.forEach((car) => {
-      const node = document.createElement("div");
-      node.innerHTML = car.render();
-      this.carContainerElement.appendChild(node);
-    });
-  };
-
-  async load() {
-    const cars = await Binar.listCars();
-    Car.init(cars);
-  }
-
-  clear = () => {
-    let child = this.carContainerElement.firstElementChild;
-
-    while (child) {
-      child.remove();
-      child = this.carContainerElement.firstElementChild;
+    constructor() {
+        this.clearButton = document.getElementById("clear-btn");
+        this.loadButton = document.getElementById("load-btn");
+        this.carCard = document.getElementById("car-card");
+        this.filterForm = document.getElementById("filter-form");
+        this.filterInputs = Array.from(
+            this.filterForm.querySelectorAll("input, select")
+        );
     }
-  };
+
+    async init() {
+        await this.load();
+        this.filterForm.addEventListener("input", this.filterCars);
+
+        // // Register click listener
+        // this.clearButton.onclick = this.clear;
+        // this.loadButton.onclick = this.run;
+    }
+
+    run = () => {
+        let carElement = "";
+        Car.list.forEach((car) => {
+            carElement += `
+              <div class="col-lg-3 d-flex">
+                  <div class="card flex-column" style="width: 18rem">
+                      <img
+                          src="${car.image}"
+                          class="card-img-top"
+                          alt="car-img"
+                          style="width: 100%; height: 200px; object-fit: cover;"
+
+                      />
+                      <div class="card-body flex-grow-1">
+                          <h5 class="card-title">${car.manufacture} ${car.model}</h5>
+                          <p class="card-text price">Rp ${car.rentPerDay} / hari</p>
+                          <p class="card-text" style="height: 7rem">
+                              ${car.description}
+                          </p>
+                          <p>
+                              <img src="img/fi_users.png" alt="users" /><span
+                                  class="ps-2"
+                                  >${car.capacity}</span
+                              >
+                          </p>
+                          <p>
+                              <img
+                                  src="img/fi_settings.png"
+                                  alt="users"
+                              /><span class="ps-2">${car.transmission}</span>
+                          </p>
+                          <p>
+                              <img
+                                  src="img/fi_calendar.png"
+                                  alt="users"
+                              /><span class="ps-2">Tahun ${car.year}</span>
+                          </p>
+                          <a href="#" class="btn btn-success btn-pilih"
+                              >Pilih Mobil</a
+                          >
+                      </div>
+                  </div>
+              </div>
+        `;
+        });
+        this.carCard.innerHTML = carElement;
+    };
+
+    filterCars = () => {
+        const filteredCars = Car.list.filter((car) => {
+            const date = this.filterForm.querySelector("#tanggal").value;
+            const passengerCount =
+                this.filterForm.querySelector("#jumlah-penumpang").value;
+            const numPassengerCount = Number(passengerCount);
+            let carCapacity = Number(car.capacity);
+
+            if (date && new Date(car.availableAt) > new Date(date)) {
+                return false;
+            }
+
+            if (passengerCount && carCapacity < numPassengerCount + 1) {
+                return false;
+            }
+
+            return true;
+        });
+
+        this.displayCars(filteredCars);
+    };
+
+    displayCars(filteredCars) {
+        let carElement = "";
+        filteredCars.forEach((car) => {
+            carElement += `
+              <div class="col-lg-3 d-flex">
+                  <div class="card flex-column" style="width: 18rem">
+                      <img
+                          src="${car.image}"
+                          class="card-img-top"
+                          alt="car-img"
+                          style="width: 100%; height: 200px; object-fit: cover;"
+                      />
+                      <div class="card-body flex-grow-1">
+                          <h5 class="card-title">${car.manufacture} ${car.model}</h5>
+                          <p class="card-text price">Rp ${car.rentPerDay} / hari</p>
+                          <p class="card-text" style="height: 7rem">
+                              ${car.description}
+                          </p>
+                          <p>
+                              <img src="img/fi_users.png" alt="users" /><span
+                                  class="ps-2"
+                                  >${car.capacity}</span
+                              >
+                          </p>
+                          <p>
+                              <img
+                                  src="img/fi_settings.png"
+                                  alt="users"
+                              /><span class="ps-2">${car.transmission}</span>
+                          </p>
+                          <p>
+                              <img
+                                  src="img/fi_calendar.png"
+                                  alt="users"
+                              /><span class="ps-2">Tahun ${car.year}</span>
+                          </p>
+                          <a href="#" class="btn btn-success btn-pilih"
+                              >Pilih Mobil</a
+                          >
+                      </div>
+                  </div>
+              </div>
+        `;
+        });
+        this.carCard.innerHTML = carElement;
+    }
+
+    async load() {
+        const cars = await Binar.listCars();
+        Car.init(cars);
+        this.displayCars(Car.list);
+    }
+
+    clear = () => {
+        let child = this.carContainerElement.firstElementChild;
+
+        while (child) {
+            child.remove();
+            child = this.carContainerElement.firstElementChild;
+        }
+    };
 }
--- public/scripts/car.example.js
+++ public/scripts/car.example.js
@@ -1,46 +1,46 @@
 class Car {
-  static list = [];
+    static list = [];
 
-  static init(cars) {
-    this.list = cars.map((i) => new this(i));
-  }
+    static init(cars) {
+        this.list = cars.map((i) => new this(i));
+    }
 
-  constructor({
-    id,
-    plate,
-    manufacture,
-    model,
-    image,
-    rentPerDay,
-    capacity,
-    description,
-    transmission,
-    available,
-    type,
-    year,
-    options,
-    specs,
-    availableAt,
-  }) {
-    this.id = id;
-    this.plate = plate;
-    this.manufacture = manufacture;
-    this.model = model;
-    this.image = image;
-    this.rentPerDay = rentPerDay;
-    this.capacity = capacity;
-    this.description = description;
-    this.transmission = transmission;
-    this.available = available;
-    this.type = type;
-    this.year = year;
-    this.options = options;
-    this.specs = specs;
-    this.availableAt = availableAt;
-  }
+    constructor({
+        id,
+        plate,
+        manufacture,
+        model,
+        image,
+        rentPerDay,
+        capacity,
+        description,
+        transmission,
+        available,
+        type,
+        year,
+        options,
+        specs,
+        availableAt,
+    }) {
+        this.id = id;
+        this.plate = plate;
+        this.manufacture = manufacture;
+        this.model = model;
+        this.image = image;
+        this.rentPerDay = rentPerDay;
+        this.capacity = capacity;
+        this.description = description;
+        this.transmission = transmission;
+        this.available = available;
+        this.type = type;
+        this.year = year;
+        this.options = options;
+        this.specs = specs;
+        this.availableAt = availableAt;
+    }
 
-  render() {
-    return `
+    render() {
+        return `
       <p>id: <b>${this.id}</b></p>
       <p>plate: <b>${this.plate}</b></p>
       <p>manufacture: <b>${this.manufacture}</b></p>
@@ -48,5 +48,5 @@ class Car {
       <p>available at: <b>${this.availableAt}</b></p>
       <img src="${this.image}" alt="${this.manufacture}" width="64px">
     `;
-  }
+    }
 }
